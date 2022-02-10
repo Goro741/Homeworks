9bd6ff8b93ffb4fff195dc6d93c9b17185c3e9d9
Goro741 <gormartirosyan57@gmail.com>
2022-02-10 19:59:15 +0400

    n'

---
+++ less21.py
@@ -0,0 +1,46 @@
+# 2.Write a python program 
+# to find max of two 
+# numbers
+# def max(a,b):
+#     if a > b:
+#         return a
+#     else:
+#         return b 
+# a = 2
+# b = 4           
+# print(max(a,b))
+
+
+# 3.Write a python program 
+# to sum all numbers
+# def sum(a,b):
+#     if a + b:
+#         return a + b
+# a = 10 
+# b = 15
+# print(sum(a,b))
+
+# 4.Write a python program 
+# to multiply all numbers
+          
+# def multiply(a,b):
+#     if a * b:
+#         return a*b 
+# x = int(input("enter the number-------"))
+# z = int(input("Enter the numnber-------"))
+# print(multiply(x,z))
+
+# 5.Write a python program 
+# to sum all letter and
+# number in your string.
+# def sum(text):
+#     tarer = 0
+#     tver = 0
+#     for i in text:
+#         if i.isdigit():
+#             tver += 1
+#         if i.isalpha():
+#             tarer += 1
+#     return tarer + tver
+# text = "Gor12547"    
+# print(sum(text))  
