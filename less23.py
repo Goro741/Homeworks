293833933ce74bad1244b44ba2ba0e717ccf25e4
Goro741 <gormartirosyan57@gmail.com>
2022-03-03 17:21:49 +0400

    New message

---
+++ less23.py
@@ -0,0 +1,39 @@
+# 4. sum wOrd
+# Given a list of strings. Find the strings with maximum and 
+# minimum lengths in array. Print the sum of their lengths
+
+# mylist = ["Armenia", "me", "Antarktida","more"]
+# n = max(mylist,key=len)
+# print(n)
+# m = min(mylist,key=len)
+# print(m)
+# print(len(n)+len(m))    
+
+
+# 6. New DicT
+# Define a function which can generate a dictionary where 
+# the keys are numbers between 1 and N (both included) and 
+# the values are square of keys. The function should print the 
+# dict.Example :
+# N = 5
+# def myfunc(number):
+#     dict1 = {}
+#     for i in range(1,number+1):
+#         dict1[i] = i**2
+#     return dict1
+# print(myfunc(5))
+
+
+# 7. INVERT DicT
+# Given an dict. Invert it (keys become values and values 
+# become keys). If there is more than key for that given value 
+# create an list.Input
+# { a: ‘1’, b: ‘2’, c: ‘2’ }
+# Output
+# { 1: ‘a’, 2: [‘b’, ‘c’] }
+
+# mydict = {"a":"1","b":"2","c":"2"}
+# new_dict ={}
+# for i,j in mydict.items():
+#     new_dict[j] = i
+# print(new_dict)
