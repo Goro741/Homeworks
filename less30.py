64ab1bf2b4b371e07ccd47e35a3c493c7d101671
Goro741 <gormartirosyan57@gmail.com>
2022-03-19 17:06:05 +0400

    new message

---
+++ less30.py
@@ -0,0 +1,17 @@
+# class Weight:
+#     def __init__(self,mylist):
+#         self.mylist = mylist
+
+#     def gumar(self):
+#         myres = 0
+#         res = 0
+#         x = self.mylist[::2]
+#         for i in x:
+#             res += i   
+#         z = self.mylist[1::2]
+#         for k in z:
+#             myres += k
+#         return res,myres        
+            
+# y = Weight([50,60,60,45,70])
+# print(y.gumar())
